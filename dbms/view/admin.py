9564dcc21cb0ca84c49069a3f5085e63a48455d1
hrsoup <654099893@qq.com>
2020-05-19 17:49:25 +0800

    教师和管理员写好了

--- dbms/view/admin.py
+++ dbms/view/admin.py
@@ -1,6 +1,7 @@
 #管理员子系统
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.db import connection
 
 def admin1(request): #个人信息
     return render(request,'admin1.html')
@@ -16,12 +17,74 @@ def admin4(request): #课程信息
 
 def indexAdmin(request):
     print("查询管理员个人信息")
+    if 'sessionid' in request.COOKIES and request.session['role'] == 'admin': 
+        admin_id = request.session['id']
+        connection.connect()  
+        cursor = connection.cursor()
+        cursor.execute("select * from admin where admin_id='%s'" % (admin_id))
+        result = cursor.fetchall()
+        connection.close()
+        result_list = []
+        for r in result:
+            result_list.append({"admin_id":r[0],'admin_name':r[2]})
+        print(result_list)
+        return redirect('/pro/admin1')
+    else:
+        print("用户身份不合法")
+        return redirect('/pro/login/')
 
 def indexAllStu(request):
     print("查询所有学生信息")
+    if 'sessionid' in request.COOKIES and request.session['role'] == 'admin': 
+        admin_id = request.session['id']
+        connection.connect()  
+        cursor = connection.cursor()
+        cursor.execute("select student.student_id,password,student_name,dept,major,class.class_id \
+                        from student natural join class;")
+        result = cursor.fetchall()
+        connection.close()
+        result_list = []
+        for r in result:
+            result_list.append({"student_id":r[0],'password':r[1],'student_name':r[2],\
+                                'dept':r[3],'major':r[4],'class_id':r[5]})
+        print(result_list)
+        return redirect('/pro/admin1')
+    else:
+        print("用户身份不合法")
+        return redirect('/pro/login/')
 
 def indexAllTeacher(request):
     print("查询所有教师信息")
+    if 'sessionid' in request.COOKIES and request.session['role'] == 'admin': 
+        teacher_id = request.session['id']
+        connection.connect()  
+        cursor = connection.cursor()
+        cursor.execute("select * from teacher;")
+        result = cursor.fetchall()
+        connection.close()
+        result_list = []
+        for r in result:
+            result_list.append({"teacher_id":r[0],"password":r[1],'teacher_name':r[2],'dept':r[3]})
+        print(result_list)
+        return redirect('/pro/admin1')
+    else:
+        print("用户身份不合法")
+        return redirect('/pro/login/')
 
 def indexAllCourse(request):
     print("查询所有课程信息")
+    if 'sessionid' in request.COOKIES and request.session['role'] == 'admin': 
+        teacher_id = request.session['id']
+        connection.connect()  
+        cursor = connection.cursor()
+        cursor.execute("select * from course;")
+        result = cursor.fetchall()
+        connection.close()
+        result_list = []
+        for r in result:
+            result_list.append({"course_id":r[0],'course_name':r[1],'credits':r[2]})
+        print(result_list)
+        return redirect('/pro/admin1')
+    else:
+        print("用户身份不合法")
+        return redirect('/pro/login/')
--- dbms/view/teacher.py
+++ dbms/view/teacher.py
@@ -1,6 +1,7 @@
 #教师子系统
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.db import connection
 
 def teacher1(request):#个人信息
     return render(request,'teacher1.html')
@@ -13,12 +14,63 @@ def teacher3(request):#学生成绩
 
 def indexTeacher(request):
     print("查询教师自己的信息")
+    if 'sessionid' in request.COOKIES and request.session['role'] == 'teacher': 
+        teacher_id = request.session['id']
+        connection.connect()  
+        cursor = connection.cursor()
+        cursor.execute("select * from teacher where teacher_id='%s'" % (teacher_id))
+        result = cursor.fetchall()
+        connection.close()
+        result_list = []
+        for r in result:
+            result_list.append({"teacher_id":r[0],'teacher_name':r[2],'dept':r[3]})
+        print(result_list)
+        return redirect('/pro/teacher1')
+    else:
+        print("用户身份不合法")
+        return redirect('/pro/login/')
 
 def indexTCourse(request):
     print("查询教师教授的课程")
+    if 'sessionid' in request.COOKIES and request.session['role'] == 'teacher': 
+        teacher_id = request.session['id']
+        connection.connect()    
+        cursor = connection.cursor()
+        cursor.execute("select course.course_id,course_name,credits \
+                        from course natural join teach \
+                        where teacher_id='%s'" % (teacher_id))
+        result = cursor.fetchall()
+        connection.close()
+        result_list = []
+        for r in result:
+            result_list.append({"course_id":r[0],'course_name':r[1],'credits':r[2]})
+        print(result_list)
+        return redirect('/pro/teacher1')
+    else:
+        print("用户身份不合法")
+        return redirect('/pro/login/')
 
 def indexTGrade(request):
     print("查询学生的成绩")
+    if 'sessionid' in request.COOKIES and request.session['role'] == 'teacher': 
+        teacher_id = request.session['id']
+        connection.connect()    
+        cursor = connection.cursor()
+        cursor.execute("select take.student_id,student_name,take.course_id,course_name,credits,grade \
+                        from student natural join course natural join take natural join teach \
+                        where teacher_id ='%s'" % (teacher_id))
+        result = cursor.fetchall()
+        connection.close()
+        result_list = []
+        for r in result:
+            result_list.append({"student_id":r[0],'student_name':r[1],'course_id':r[2],\
+                                'course_name':r[3],'credits':r[4],'grade':r[5]})
+        print(result_list)
+        return redirect('/pro/teacher1')
+    else:
+        print("用户身份不合法")
+        return redirect('/pro/login/')
 
 def indexTDist(request):
     print("查询学生成绩分布")
+
